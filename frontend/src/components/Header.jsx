d4af22a2a33a27c90ea14a3068c060345d292312
Yash Kothari <yashkothari@Yashs-MacBook-Air.local>
2025-01-17 18:47:29 +0530

    Added Sign up and Log in functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,73 @@
-import { Badge, Navbar, Nav, Container} from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useSelector } from 'react-redux';
-import logo from '../assets/logo.png';
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from '../slices/authSlice';
+import logo from "../assets/logo.png";
 
 const Header = () => {
-    const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
 
-    return (
-        <header>
-            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
-                <Container>
-                    <LinkContainer to="/">
-                        <Navbar.Brand>
-                            <img src={logo} alt='Shopify' />
-                            Shopify
-                        </Navbar.Brand>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+        await logoutApiCall().unwrap();
+        dispatch(logout());
+        navigate('/login');
+    } catch (err) {
+        console.log(err)
+    }
+  }
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="Shopify" />
+              Shopify
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+              { userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
                     </LinkContainer>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ms-auto'>
-                            <LinkContainer to='/cart'>
-                                <Nav.Link>
-                                    <FaShoppingCart /> Cart
-                                    {
-                                        cartItems.length > 0 && (
-                                            <Badge pill bg='success' style={{marginLeft:'5px'}}>
-                                                { cartItems.reduce((a, c) => a + c.qty, 0)}
-                                            </Badge>
-                                        )
-                                    }
-                                </Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <FaUser /> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
-}
-export default Header
+                    <NavDropdown.Item onClick={logoutHandler}>
+                        Logout
+                    </NavDropdown.Item>
+                </NavDropdown>
+              ) : (<LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>) }
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
