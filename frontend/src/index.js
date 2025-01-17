cef97b71c7a0cc9fdd0a8289f9063ed4618914c2
Yash Kothari <yashkothari@Yashs-MacBook-Air.local>
2025-01-18 00:15:20 +0530

    Fixed bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
