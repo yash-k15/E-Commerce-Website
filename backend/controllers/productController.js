b072fd54275e507d300b2020129a6335df0b9c12
Yash Kothari <yashkothari@Yashs-MacBook-Air.local>
2025-01-23 00:51:00 +0530

    Added reviews functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -44,7 +44,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
 // @desc Update a product
 // @route PUT /api/product/:id
-// @access Purivate/Admin
+// @access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
   const { name, price, description, image, brand, category, countInStock } =
     req.body;
@@ -70,7 +70,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 // @desc Delete a product
 // @route DELETE /api/product/:id
-// @access Purivate/Admin
+// @access Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
@@ -83,10 +83,49 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Create a new review
+// @route POST /api/product/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(404);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
